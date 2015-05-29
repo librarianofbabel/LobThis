20b0bf88a13959d2c2936c8adb6ada4ed7531d4b
librarianofbabel <jonathan.e.basile@gmail.com>
2015-05-29 08:25:55 -0400

    Update LobThis.js

--- LobThis.js
+++ LobThis.js
@@ -1,5 +1,15 @@
-var a = document.createElement('a');
-a.href = "http://libraryofbabel.info/resourcelocator.cgi";
-a.target = "_blank";
-document.body.appendChild(a);
-a.click();
+var bodytext = document.body;
+var formext = document.createElement("form");
+formext.setAttribute("method", "POST");
+formext.setAttribute("action", "http://libraryofbabel.info/resourcelocator.cgi");
+formext.setAttribute("target", "_blank");
+
+var hiddenField = document.createElement("input");
+hiddenField.setAttribute("type", "hidden");
+hiddenField.setAttribute("name", "extension");
+hiddenField.setAttribute("value", bodytext.innerHTML);
+
+console.log(bodytext);
+formext.appendChild(hiddenField);
+document.body.appendChild(formext);
+formext.submit();
